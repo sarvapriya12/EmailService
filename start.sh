2b6ba76bf6d39268a277452abcd4aef118439aa9
Sarvapriya Harsh <spriyaharsh@gmail.com>
2026-06-09 21:23:14 +0530

    reduce celery memory: solo pool, concurrency=1

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-celery -A celery_app worker --loglevel=info &
+celery -A celery_app worker --loglevel=info --pool=solo --concurrency=1 &
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-8080}
